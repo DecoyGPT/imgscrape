26a639f0404c73fb77f43280b95379f558a39da2
Decoy <134445092+DecoyGPT@users.noreply.github.com>
2023-09-11 15:07:11 -0400

    Updates
    - Add ability to specify filename
    - Add shopify regex for file sizes

--- ImgScrape.py
+++ ImgScrape.py
@@ -1,30 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import re
 import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from urllib.parse import urljoin
+
+def get_full_size_image_url(img_url):
+    """
+    Convert thumbnail image URL to full size URL based on known patterns.
+    Current patterns handled: Shopify's CDN URLs with size information.
+    """
+    shopify_pattern = re.compile(r'_(\d+x)\.png')
+    if shopify_pattern.search(img_url):
+        img_url = shopify_pattern.sub('.png', img_url)
+    return img_url
 
 def extract_images_from_url(url, delay=10, user_agent=None):
-    options = Options()
+    options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    if user_agent:
-        options.add_argument(f"user-agent={user_agent}")
-
-    print("Launching headless browser to navigate the URL...")
+    options.add_argument(f'user-agent={user_agent}')
+    
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    
-    # Wait for the content to load. Adjust the delay as needed.
-    print(f"Waiting for {delay} seconds to ensure content is loaded...")
-    time.sleep(delay)
+    time.sleep(delay)  # Let JS-loaded content load
     
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
 
     img_tags = soup.find_all('img')
     print(f"Found {len(img_tags)} image(s) on the page.")
-    images = [urljoin(url, img['src']) for img in img_tags if img.has_attr('src')]
+    
+    # Get full size image URLs
+    images = [get_full_size_image_url(urljoin(url, img['src'])) for img in img_tags if img.has_attr('src')]
+    
     return images
 
 def create_html_with_images(image_list, output_file):
@@ -38,6 +45,7 @@ def create_html_with_images(image_list, output_file):
 
 if __name__ == "__main__":
     url = input("Enter the URL: ")
+    output_filename = input("Enter the desired output filename (e.g., output.html): ")
 
     # Provide a commonly used user agent
     user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
@@ -46,7 +54,7 @@ if __name__ == "__main__":
     images = extract_images_from_url(url, user_agent=user_agent)
     
     if images:
-        create_html_with_images(images, "output.html")
-        print("Check output.html for the images.")
+        create_html_with_images(images, output_filename)
+        print(f"Check {output_filename} for the images.")
     else:
         print("No images found!")
